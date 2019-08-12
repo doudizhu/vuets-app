5f7abc0ced287c2c4ce9400caceff4af3b374114
lipeng <lipeng@daokoudai.com>
2019-08-12 17:43:46 +0800

    2.VueCli3+Ts实战项目-文件结构目录

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang='pug'>
+  .home home
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+<script>
+import {Component,Vue} from 'vue-property-decorator'
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components:{}
 })
 export default class Home extends Vue {}
 </script>
