6a9e764c724212f12738b659bfd4a0fbac079781
lipeng <lipeng@daokoudai.com>
2019-08-17 16:44:12 +0800

    22.VueCli3+Ts实战项目-渲染账户管理页面

--- src/views/UserManage/AccountData.vue
+++ src/views/UserManage/AccountData.vue
@@ -1,11 +1,47 @@
 <template lang='pug'>
-  .home AccountData
+.account-data
+  .add-
+    el-button(type='primary') 新增账户
+  el-table(:data='tableData' border style='width:100%;')
+    el-table-column(label='角色' width='180')
+      template(slot-scope='scope')
+        span {{scope.row.role}}
+    el-table-column(label='账号' width='180')
+      template(slot-scope='scope')
+        span {{scope.row.username}}
+    el-table-column(prop='des' label='描述' width='180')
+    el-table-column(label='操作' width='180')
+      template(slot-scope='scope' v-if='scope.row.username != "admin"')
+        el-button(size='mini') 编辑
+        el-button(size='mini' type='danger') 删除
 </template>
 
 <script lang='ts'>
-import {Component,Vue} from 'vue-property-decorator'
+import {Component,Vue,Provide} from 'vue-property-decorator'
 @Component({
   components:{}
 })
-export default class AccountData extends Vue {}
+export default class AccountData extends Vue {
+  @Provide() tableData:any = []
+
+  created(){
+    this.getData();
+  }
+
+  getData(){
+    (this as any).$axios('/api/users/allUsers').then((res:any)=>{
+      this.tableData = res.data.datas;
+    })
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.account-data {
+  height: 100%;
+  overflow: auto;
+  .add-box {
+    margin-bottom: 10px;
+  }
+}
+</style>
